11ad0b0ff45a64db63b167295626520975065af6
Nick Bakkegaard <nickbakkegaard@gmail.com>
2017-11-29 15:22:21 +0100

    add sorting

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ let apps_url =
 
 
 $(function(){
-	let apps_info = (function(){
+	let apps_info =  [];
+	(function(){
 		let temp = []
 		let counter = 0;
 		for(let i=0;i<apps_url.length;i++){
@@ -15,6 +16,7 @@ $(function(){
 					temp.push(data)
 					counter++;
 					if(counter==apps_url.length){
+						apps_info = temp;
 						buildTable(temp);
 					}
 				})
@@ -23,12 +25,22 @@ $(function(){
 	})()
 	function buildTable(list){
 		let el = $("#tb");
+		el.empty();
 		for(let i = 0;i<list.length;i++){
 			let current = $("<tr></tr>");
-			current.append($("<td>" + list[i].name + "</td>"));
+			current.append($("<td><a href=\"https://github.com/" + list[i].full_name + "\">" + list[i].name +"</a></td>"));
 			current.append($("<td>" + list[i].stargazers_count + "</td>"));
 			current.append($("<td>" + list[i].subscribers_count + "</td>"));
 			el.append(current);
 		}
 	}
+	$("th").click(function(e){
+		let name = e.target.innerHTML;
+		let selector = "";
+		if(name == "Name") selector="name";
+		else if (name == "Stars") selector = "stargazers_count";
+		else if (name == "Watchers") selector = "subscribers_count";
+		buildTable(apps_info.sort(function(e1,e2){return e1[selector]<e2[selector]}));
+			
+	})
 })
