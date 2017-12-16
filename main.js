924544b64596cbb2761d9c34fad1c6fe0b972b81
Nick Bakkegaard <nickbakkegaard@gmail.com>
2017-12-16 17:53:36 +0100

    change to Vue.js framework

--- main.js
+++ main.js
@@ -1,53 +1,59 @@
 let apps_url = 
   [
-	"nextcloud/calendar",
-	"nextcloud/contacts",
 	"nextcloud/deck",
 	"nextcloud/news",
 	"nextcloud/notes",
+	"nextcloud/calendar",
+	"nextcloud/contacts",
 	"nextcloud/passman",
 	"nextcloud/polls",
 	"nextcloud/spreed",
   ]
 
-
-
 $(function(){
-	let apps_info =  [];
 	(function(){
 		let temp = []
 		let counter = 0;
 		for(let i=0;i<apps_url.length;i++){
 			$.ajax( "https://api.github.com/repos/"+apps_url[i])
 				.done(function(data) {
-					temp.push(data)
-					counter++;
-					if(counter==apps_url.length){
-						apps_info = temp;
-						buildTable(temp);
-					}
+					table.apps.push(data);
 				})
 		}
-		return temp;
 	})()
-	function buildTable(list){
-		let el = $("#tb");
-		el.empty();
-		for(let i = 0;i<list.length;i++){
-			let current = $("<tr></tr>");
-			current.append($("<td><a href=\"https://github.com/" + list[i].full_name + "\">" + list[i].name +"</a></td>"));
-			current.append($("<td>" + list[i].stargazers_count + "</td>"));
-			current.append($("<td>" + list[i].subscribers_count + "</td>"));
-			el.append(current);
-		}
-	}
+
 	$("th").click(function(e){
 		let name = e.target.innerHTML;
 		let selector = "";
 		if(name == "Name") selector="name";
 		else if (name == "Stars") selector = "stargazers_count";
 		else if (name == "Watchers") selector = "subscribers_count";
-		buildTable(apps_info.sort(function(e1,e2){return e1[selector]<e2[selector]}));
+		table.apps.sort(function(e1,e2){
+					
+			if(e1[selector]<e2[selector]){
+				return -1;
+			}
+			else if(e1[selector]>e2[selector]){
+				return 1;
+			}
+			else{
+				return 0;
+			}
+		})
 			
 	})
+
+	table = new Vue({
+		el: '#table',
+		data: {
+			apps: []
+		},
+		methods:{
+			getURL:function(app){
+				return "https://github.com/"+ app.full_name;
+			}
+		}
+
+	})
+
 })
